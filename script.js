7bde9054cbe8d462adb793916d34aec62b60f705
nasim-kuniyil <md.nasimkuniyil.com>
2024-03-29 06:26:38 +0530

    update social media links and submit btn disabled form

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 function validform(){
     var username = document.contactform.fname.value
-    var usermail = document.contactform.email.value
-    var usermobile = document.contactform.mobile.number
 
-    if(username[0] == " " || username.includes(1) || username.includes(2) || username.includes(3) || username.includes(4)
+    if(username[0] == " " || username == null || username.includes(1) || username.includes(2) || username.includes(3) || username.includes(4)
     || username.includes(5) || username.includes(6) || username.includes(7) || username.includes(8) || username.includes(9) || username.includes(0)){
         alert("Correct Your Name"); 
+        document.getElementById("submitbtn").disabled = true;
         return false;
+    }else{
+        document.getElementById("submitbtn").disabled = false;
     }
 
 }
